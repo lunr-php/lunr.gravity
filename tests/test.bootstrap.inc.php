87dc1210991a152ffe2cb560ecf47f13cf258856
Heinz Wiesinger <heinz@m2mobi.com>
2012-03-11 21:52:45 +0100

    tests: Fix codestyle issues

--- tests/test.bootstrap.inc.php
+++ tests/test.bootstrap.inc.php
@@ -1,6 +1,20 @@
 <?php
 
-$base = dirname(__FILE__) . "/..";
+/**
+ * PHPUnit bootstrap file.
+ *
+ * Set include path and initialize autoloader.
+ *
+ * PHP Version 5.3
+ *
+ * @category   Loaders
+ * @package    Tests
+ * @subpackage Tests
+ * @author     M2Mobi <info@m2mobi.com>
+ * @author     Heinz Wiesinger <heinz@m2mobi.com>
+ */
+
+$base = dirname(__FILE__) . '/..';
 
 set_include_path(
     $base . "/config:" .
@@ -10,7 +24,7 @@ set_include_path(
 );
 
 // Load and setup class file autloader
-include_once("libraries/core/class.autoloader.inc.php");
+require_once 'libraries/core/class.autoloader.inc.php';
 spl_autoload_register(array(new Lunr\Libraries\Core\Autoloader(), 'load'));
 
 ?>
