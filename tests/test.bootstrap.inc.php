9373e84ac34b8b4ca370852fb47ca9ce38f54edf
Heinz Wiesinger <heinz@m2mobi.com>
2012-04-28 15:06:03 +0200

    tests: Adjust include path in bootstrap file.
    Add new path to be able to find mocks

--- tests/test.bootstrap.inc.php
+++ tests/test.bootstrap.inc.php
@@ -17,9 +17,10 @@
 $base = dirname(__FILE__) . '/..';
 
 set_include_path(
-    $base . "/config:" .
-    $base . "/system:" .
-    $base . "/tests:" .
+    $base . '/config:' .
+    $base . '/system:' .
+    $base . '/tests:' .
+    $base . '/tests/mocks:' .
     get_include_path()
 );
 
