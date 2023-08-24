89d8360bcedf7cf1e67b23f20816606cdf7e4ba3
Heinz Wiesinger <pprkut@liwjatan.org>
2023-08-24 16:55:55 +0200

    MySQL: Add spaces after comma in UUID escaping function call
    Reviewed at https://reviews.lunr.nl/r/1033/

--- src/Lunr/Gravity/MySQL/Tests/MySQLQueryEscaperEscapeUUIDValueTest.php
+++ src/Lunr/Gravity/MySQL/Tests/MySQLQueryEscaperEscapeUUIDValueTest.php
@@ -32,7 +32,7 @@ class MySQLQueryEscaperEscapeUUIDValueTest extends MySQLQueryEscaperTest
                       ->method('escape_string')
                       ->will($this->returnValue('value'));
 
-        $this->assertEquals("UNHEX(REPLACE('value','-',''))", $this->class->uuidvalue('value'));
+        $this->assertEquals("UNHEX(REPLACE('value', '-', ''))", $this->class->uuidvalue('value'));
     }
 
     /**
@@ -47,7 +47,7 @@ class MySQLQueryEscaperEscapeUUIDValueTest extends MySQLQueryEscaperTest
                       ->method('escape_string')
                       ->will($this->returnValue('value'));
 
-        $string = "UNHEX(REPLACE('value','-','')) COLLATE utf8_general_ci";
+        $string = "UNHEX(REPLACE('value', '-', '')) COLLATE utf8_general_ci";
 
         $this->assertEquals($string, $this->class->uuidvalue('value', 'utf8_general_ci'));
     }
@@ -64,7 +64,7 @@ class MySQLQueryEscaperEscapeUUIDValueTest extends MySQLQueryEscaperTest
                       ->method('escape_string')
                       ->will($this->returnValue('value'));
 
-        $this->assertEquals("ascii UNHEX(REPLACE('value','-',''))", $this->class->uuidvalue('value', '', 'ascii'));
+        $this->assertEquals("ascii UNHEX(REPLACE('value', '-', ''))", $this->class->uuidvalue('value', '', 'ascii'));
     }
 
     /**
@@ -79,7 +79,7 @@ class MySQLQueryEscaperEscapeUUIDValueTest extends MySQLQueryEscaperTest
                       ->method('escape_string')
                       ->will($this->returnValue('value'));
 
-        $string = "ascii UNHEX(REPLACE('value','-','')) COLLATE utf8_general_ci";
+        $string = "ascii UNHEX(REPLACE('value', '-', '')) COLLATE utf8_general_ci";
 
         $this->assertEquals($string, $this->class->uuidvalue('value', 'utf8_general_ci', 'ascii'));
     }
